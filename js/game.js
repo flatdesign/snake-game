abc138c7a20e068eb7b6a231216d4f02eac15255
Danil Chushko <danil.krosis@gmail.com>
2021-01-06 18:25:28 +0300

    fix score bug

--- js/game.js
+++ js/game.js
@@ -9,14 +9,14 @@ foodImg.src = "img/food.png";
 
 const box = 32;
 
-const score = 0;
+let score = 0;
 
-const food = {
+let food = {
   x: Math.floor((Math.random() * 17 + 1)) * box,
   y: Math.floor((Math.random() * 15 + 3)) * box,
 };
 
-const snake = [];
+let snake = [];
 snake[0] = {
   x: 9 * box,
   y: 10 * box
