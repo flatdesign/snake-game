4fe5dabca0a017ce3b3bd060b5368282ca140bc8
Danil Chushko <danil.krosis@gmail.com>
2021-01-07 12:02:19 +0300

    add JSDoc documentation

--- js/game.js
+++ js/game.js
@@ -1,42 +1,63 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
 
+/** DOM container */
+const gameContainer = document.getElementById("game");
+/** 2d context, which we use for drawing */
+const ctx = gameContainer.getContext("2d");
+
+/** groung image */
 const ground = new Image();
 ground.src = "img/ground.png";
 
+/** food image */
 const foodImg = new Image();
 foodImg.src = "img/food.png";
 
+/** size of 1 box element in px */
 const box = 32;
 
+/** users score */
 let score = 0;
 
+/** coordinates for food image */
 let food = {
   x: Math.floor((Math.random() * 17 + 1)) * box,
   y: Math.floor((Math.random() * 15 + 3)) * box,
 };
 
+/** coordinates for snake */
 let snake = [];
 snake[0] = {
   x: 9 * box,
   y: 10 * box
 };
 
+/** current direction */
 let dir;
 
+/**
+ * Give keydown event and change current direction.
+ * @param {KeyboardEvent} event - keydown event.
+ * @return {undefined}
+ */
 const direction = (event) => {
-  if(event.keyCode === 37 && dir !== "right")
+  if(event.key === 'ArrowLeft' && dir !== "right")
     dir = "left";
-  else if(event.keyCode === 38 && dir !== "down")
+  else if(event.key === 'ArrowUp' && dir !== "down")
     dir = "up";
-  else if(event.keyCode === 39 && dir !== "left")
+  else if(event.key === 'ArrowRight' && dir !== "left")
     dir = "right";
-  else if(event.keyCode === 40 && dir !== "up")
+  else if(event.key === 'ArrowDown' && dir !== "up")
     dir = "down";
 }
 
 document.addEventListener("keydown", direction);
 
+/**
+ * Сheck that the snake has not eaten itself. Stop the game if she ate her tail.
+ * @param {{x: number, y: number}} head - coordinates of snake head.
+ * @param {{x: number, y: number}[]} arr - coordinates of snake tail.
+ * @return {undefined}
+ */
 const checkEatTail = (head, arr) => {
   for(let i = 0; i < arr.length; i++) {
     if(head.x == arr[i].x && head.y == arr[i].y)
@@ -44,6 +65,10 @@ const checkEatTail = (head, arr) => {
   }
 }
 
+/**
+ * Main function which render all entities
+ * @return {undefined}
+ */
 const drawGame = () => {
   ctx.drawImage(ground, 0, 0);
 
@@ -58,7 +83,9 @@ const drawGame = () => {
   ctx.font = "50px Arial";
   ctx.fillText(score, box * 2.5, box * 1.7);
 
+  /** current location of snake head in X coordinate*/
   let snakeX = snake[0].x;
+  /** current location of snake head in Y coordinate*/
   let snakeY = snake[0].y;
 
   if(snakeX == food.x && snakeY == food.y) {
@@ -80,6 +107,7 @@ const drawGame = () => {
   if(dir == "up") snakeY -= box;
   if(dir == "down") snakeY += box;
 
+  /** coordinates for new snake head */
   let newHead = {
     x: snakeX,
     y: snakeY
@@ -90,4 +118,5 @@ const drawGame = () => {
   snake.unshift(newHead);
 }
 
+/** timerId to stop the game */
 let game = setInterval(drawGame, 100);
